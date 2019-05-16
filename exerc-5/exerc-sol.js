15c25bc3b09cbe6533954ec1f509de888897d111
Alberto Pastor Moreno <32735087+albertopastormr@users.noreply.github.com>
2019-05-16 13:06:46 +0200

    Update exerc-sol.js (finished)

--- exerc-5/exerc-sol.js
+++ exerc-5/exerc-sol.js
@@ -93,12 +93,12 @@ var x = db.aficiones.aggregate([
 //
 
 // Consulta 1:
-// Muestra todos los elementos que tengan una puntuacion mayor de 5 y la calidad sea superior a un 60, agrupado por el genero. Mostrar solo el nombre y paginas.
-// No queremos ver el id.
+// Muestra todos los elementos que tengan una puntuacion mayor de 5 y la calidad sea superior a un 60, agrupado por el genero.
+//    Mostrar solo el nombre y paginas. No queremos ver el id.
 
 db.aficiones.aggregate([
-    {$match: { $and:[ Puntuacion: {$gte:5}, Calidad: {$gte:60}] }},
-    {$group: {_id: "$Genero", Nombres: {$push:"Nombres"}, Paginas: {$push:"$Paginas"}}},
+    {$match: { $and: [ {Puntuacion: {$gte:5}}, {Calidad: {$gte:60}}] }},
+    {$group: {_id: "$Genero", Nombres: {$push:"$Nombre"}, Paginas: {$push:"$Pagina"}}},
     {$project : {_id:0}}
 ]);
 
@@ -108,9 +108,9 @@ db.aficiones.aggregate([
 
 db.aficiones.aggregate([
     {$match: { Puntuacion: {$gte:7} }},
-    {$group: {_id: "$Genero", NumPaginas: {$sum:1}}},
-    {$project : {_id:0}}
+    {$group: {_id: "$genero", NumPaginas: {$sum:"$paginas"}}}
 ]);
+    db.aficiones.find({genero:null});
 
 // Consulta 3:
 // Muestra el precio que deberia pagar para comprar todos los libros de cada uno de los autores.
@@ -123,33 +123,38 @@ db.aficiones.aggregate([
 // Mostrar el nombre de todos los futbolistas agrupando por nacionalidad.
 
 db.aficiones.aggregate([
-    {$match: { Tema: "Fútbol"},
-    {$group: {_id: "$Caracteristicas.Nacionalidad", Nombres: {$push:"Nombres"}}}
+    {$match: { Tema:"Fútbol"}},
+    {$group: { _id: "$Caracteristicas.Nacionalidad", Nombres: {$push:"$Nombre"}}}
 ]);
     
 // Apartado e
 //
-// Tantop find() como aggregate() devuelven un cursor
+// Tanto find() como aggregate() devuelven un cursor
 db.aficiones.aggregate([
-    {$group: {_id:"$Tema"}}
+    {$group: {_id:"$Tema", Nombres: {$push:"$Nombre"}, Apodos: {$push:"$Apodo"}, Puntuaciones: {$push:"$Puntuacion"}}}
 ]).forEach(element => {
-    print("Tema: " + element.Tema + " Apodo: " + element.Apodo +
-     " Nombre: "+ element.Nombre + " Puntuacion: " +  element.Puntuacion);
+    print("Tema: " + element._id + "\n" + "  Apodo: " + element.Apodos + "\n" +
+     "  Nombre: "+ element.Nombres + "\n" + "  Puntuacion: " +  element.Puntuaciones);
 });
 
 // Apartado f
 //
-db.aficiones.update(
-    {Puntuacion:{$lt:7}},
-    { $set: { Precio: Precio*0.9, Descuento: Precio*0.1} },
+db.aficiones.find(
+    {Puntuacion:{$lt:7}}
+).forEach(element => {
+    db.aficiones.update(
+    {_id: element._id},
+    { $set: { Precio: element.Precio*0.9, Descuento: element.Precio*0.1} },
     { multi: true }
 )
+});
 
 // Apartado g
 //
+db.PorNivel.drop(); // Por si existiera de pruebas anteriores
 db.createCollection("PorNivel");
 // Insertamos la estructura de los documentos
-db.PorNivel.insert(
+db.PorNivel.insertMany([
     {
         "NomCal": "intervalo1",
         "Componentes": []
@@ -166,7 +171,7 @@ db.PorNivel.insert(
         "NomCal": "intervalo4",
         "Componentes": []
     }
-)
+]);
 // Funcion que calcula el nivel de calidad de un elemento
 function nivelCalidad(puntuacion, precio){
     return Math.round((puntuacion * precio) / 100);
@@ -189,23 +194,30 @@ db.aficiones.find().forEach(element => {
         nomCal = "intervalo4";
     }
     db.PorNivel.update(
-        { NombreCal: nomCal },
+        { NomCal: nomCal },
         { $push: { Componentes: { Comp: element, Cal: nivCal}}}
     );
 });
 
 db.PorNivel.find();
 
-db.PorNivel.find({}, {_id:0, NomCal:1, Componentes.Comp.Precio:1, Componentes.Comp.Nombre:1}).sort({"Componentes.Comp.Precio":1});
+db.PorNivel.find(
+    {},
+    {_id:0, NomCal:1, "Componentes.Comp.Precio":1, "Componentes.Comp.Nombre":1}
+).sort({"Componentes.Comp.Precio":1});
 
 // Apartado 4
 //
 db.createCollection("SuperGuai");
 // Inserta los 5 elementos de la coleccion "aficiones" que tienen el nivel de calidad
 // mas alto
-db.SuperGuai.insert(
+db.SuperGuai.insertMany(
     db.aficiones.find().sort({ nivelCalidad(Puntuacion, Precio): -1}).limit(5);
 );
+    
+db.aficiones.find().forEach(element => {
+     db.aficiones.find().sort({ nivelCalidad(element.Puntuacion, element.Precio): -1}).limit(5);
+});
 
 // Apartado 5
 //
@@ -228,7 +240,8 @@ db.apartado5.insert(
         "%Edulcorante": 15,
         "%Chocolate": 30
     }
-)
+);
+db.apartado5.find();
 // 5.Extra
 // - Conviene desnormalizar ya que asi en una sola consulta obtendriamos todas las propiedades del objeto en cuestion
 //      ganando en eficiencia.
@@ -238,22 +251,44 @@ db.apartado5.insert(
 // Apartado B
 db.aficiones.find(
     {
-        Nombre: "MotoGP"
+        Tema: "MotoGP"
     }
 ).forEach(function(element) {
     db.collection.update(
         { "_id": element._id }, 
-        { "$set": { "Nombre": element.NombreEquipo, "Puntuacion": element.Puntuacón, "Precio":100 }, "$unset": { "NombreEquipo":1, "Puntuacón":1} }
+        { "$set":
+            { "Nombre": element.NombreEquipo, "Puntuacion": element.Puntuacón, "Precio":100 },
+         "$unset": { "NombreEquipo":1, "Puntuacón":1}
+        },
+        false, // upsert
+        true // multi
+    );
+    print(element);
+});
+db.aficiones.find({ Tema: "MotoGP"});
+db.aficiones.find(
+         { Tema:{$in:["Fútbol", "Baloncesto"]}}
+).forEach(function(element) {
+    var nuevoPrecio = 0;
+    if(element.Precio > 1000){
+            nuevoPrecio = element.Precio / 10;
+        }
+    db.collection.update(
+        { "_id": element._id }, 
+        { "$set": { "Precio": nuevoPrecio} }
     );
 });
 db.aficiones.find(
     {
-        Nombre: "Ajedrez"
+        Tema: "Ajedrez"
     }
 ).forEach(function(element) {
     db.collection.update(
         { "_id": element._id }, 
-        { "$set": { "Nombre": element.Nombre + element.Apellidos, "Precio":100 }, "$unset": { "Apellidos":1} }
+        { 
+            "$set": { "Nombre": element.Nombre + element.Apellidos, "Precio":100 },
+            "$unset": { "Apellidos":""} 
+        }
     );
 });
 // Apartado C
@@ -293,37 +328,53 @@ db.aficiones.find().forEach(element => {
     // esto da todas las claves del documento element -> for (key in element) print(key);
     db.solodatos.insert(
         {
-            _id: element._id
-            
-        }
-    );
-    db.soloaficiones.insert(
-        {
-            _id: element._id,
-            Tema: element.Tema,
-            Nombre: element.Nombre,
-            Apodo: element.Apodo,
-            Precio: element.Precio
+            element
         }
     );
 });
-
+db.solodatos.find().forEach(element => {
+    db.soloaficiones.update(
+        {_id: element._id},
+        {
+            $unset:{
+                "element._id": "",
+                "element.Tema": "",
+                "element.Nombre": "",
+                "element.Apodo": "",
+                "element.Puntuacion": "",
+                "element.Precio": ""
+            }
+        }
+    );
+});
+db.solodatos.find().forEach(element => {
+    db.soloaficiones.insert(
+        {
+            _id: element._id,
+            Tema: element.element.Tema,
+            Nombre: element.element.Nombre,
+            Apodo: element.element.Apodo,
+            Precio: element.element.Precio
+        }
+);
+});
+db.soloaficiones.find();
 // Apartado E
-db.solodatos.find().array.forEach(element => {
+db.solodatos.find().forEach(element => {
     var x = db.soloaficiones.findOne( { "_id": element._id });
     print(element)
     print(x)
 });
 
 // Apartado F
-db.solodatos.aggregate([
+db.soloaficiones.aggregate([
     {
       $lookup:
         {
-          from: "soloaficiones",
+          from: "solodatos",
           localField: "_id",
           foreignField: "_id",
           as: "detalles"
         }
    }
- ])
+ ]);
